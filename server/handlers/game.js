99eba3dd5ee1eeb642ba0bcc55073227d945015f
Nachop51 <nachoperalta0@gmail.com>
2024-05-04 18:43:19 -0300

    2 players working now

--- server/handlers/game.js
+++ server/handlers/game.js
@@ -1,4 +1,4 @@
-const moves = []
+import { GAME_SOCKET_EVENTS } from '../constants.js'
 
 /**
  *
@@ -6,28 +6,35 @@ const moves = []
  * @param {import('socket.io').Socket} socket
  */
 export default function (io, socket) {
-  socket.on('game:createRoom', () => {
+  socket.on(GAME_SOCKET_EVENTS.CREATE_GAME, () => {
     const roomId = crypto.getRandomValues(new Uint32Array(1))[0].toString(16)
 
-    console.log({ roomId })
-
-    socket.leave(socket.room)
     socket.join(roomId)
 
     socket.room = roomId
 
-    socket.emit('game:roomCreated', roomId)
+    socket.emit(GAME_SOCKET_EVENTS.GAME_CREATED, roomId)
   })
 
-  socket.on('game:joinRoom', (roomId) => {
-    console.log('Joining room:', roomId)
-
-    socket.leave(socket.room)
-
+  socket.on(GAME_SOCKET_EVENTS.JOIN_ROOM, (roomId) => {
     const room = io.sockets.adapter.rooms.get(roomId)
 
     if (!room) {
-      socket.emit('game:roomNotFound', roomId)
+      socket.emit(GAME_SOCKET_EVENTS.ROOM_NOT_FOUND, roomId)
+      return
+    }
+
+    if (room.size === 1 && room.has(socket.id)) {
+      return
+    }
+
+    if (room.size > 1 && !room.has(socket.id)) {
+      console.log('Room is full')
+      socket.emit(GAME_SOCKET_EVENTS.ROOM_NOT_FOUND, roomId)
+      return
+    }
+
+    if (room.has(socket.id)) {
       return
     }
 
@@ -35,21 +42,35 @@ export default function (io, socket) {
 
     socket.room = roomId
 
-    socket.to(roomId).emit('game:roomJoined', roomId)
+    console.log({ room })
+
+    const first = Array.from(room.values())[0]
+    const second = socket.id
+
+    const firstTurn = Math.random() > 0.5 ? 'X' : 'O'
+
+    const players = {
+      [first]: firstTurn,
+      [second]: firstTurn === 'X' ? 'O' : 'X'
+    }
+
+    // console.log({ players })
+
+    // Emit to all sockets in the room
+    io.sockets.in(roomId).emit(GAME_SOCKET_EVENTS.ALL_PLAYERS_READY, players)
   })
 
-  socket.on('game:newMove', (move) => {
+  socket.on(GAME_SOCKET_EVENTS.LEAVE_ROOM, (roomId) => {
+    socket.leave(roomId)
+  })
+
+  socket.on(GAME_SOCKET_EVENTS.NEW_MOVE, (move) => {
     console.log(move)
 
-    moves.push(move)
-
-    socket.broadcast.emit('game:newMove', move)
+    socket.to(socket.room).emit(GAME_SOCKET_EVENTS.NEW_MOVE, move)
   })
 
-  socket.on('game:reset', () => {
-    console.log({ moves })
-    moves.length = 0
-
-    io.emit('game:reset')
+  socket.on(GAME_SOCKET_EVENTS.RESET, () => {
+    socket.to(socket.room).emit(GAME_SOCKET_EVENTS.RESET)
   })
 }
